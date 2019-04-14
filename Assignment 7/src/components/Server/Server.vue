8ed655a9699cbdc1da3eb26249ddba032a69c764
aventador2018 <zl272@njit.edu>
2019-04-14 12:44:43 -0400

    Components

---
+++ Assignment 7/src/components/Server/Server.vue
@@ -0,0 +1,25 @@
+<template>
+    <li
+            class="list-group-item"
+            @click="onClick"
+            style="cursor: pointer">
+        Server #{{ server.id }}
+    </li>
+</template>
+
+<script>
+    import { eventBus } from '../../main';
+
+    export default {
+        props: ['server'],
+        methods: {
+            onClick: function() {
+                eventBus.$emit('serverClicked', this.server);
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Assignment 7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">Please select a server!</p>
+        <div v-else>
+            <p>Server ID: {{ server.id }}</p>
+            <p>Server Status: {{ server.status }}</p>
+            <button @click="resetStatus">Reset Status</button>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    import { eventBus } from '../../main.js';
+
+    export default {
+        data: function() {
+            return {
+                server: null
+            }
+        },
+        created() {
+            eventBus.$on('serverClicked', (server) => {
+                this.server = server;
+            })
+        },
+        methods: {
+            resetStatus: function() {
+                this.server.status = 'Normal';
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
